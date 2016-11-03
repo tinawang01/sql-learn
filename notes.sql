87d4b00dfd3e9153cefa244fc454d167456cfd60
tinawang01 <ting.wang.dr@gmail.com>
2016-11-03 00:08:31 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -14,3 +14,40 @@ FROM nobel
 WHERE yr = 1984
 ORDER BY subject IN ('Chemistry', 'Physics'), subject, winner
 
+#SELECT within SELECT Tutorial
+#1
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+      
+#2
+SELECT name FROM world WHERE continent='Europe' AND gdp/population>(SELECT gdp/population FROM world WHERE name='United Kingdom')
+
+#3
+SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name IN ('Argentina', 'Australia')) ORDER BY name
+
+#4
+SELECT name,population FROM world WHERE population>(SELECT population FROM world WHERE name='Canada') and population<(SELECT population FROM world WHERE name='Poland')
+
+#5
+SELECT name, CONCAT(ROUND(population*100/(SELECT population FROM world WHERE name='Germany')), '%') FROM world WHERE continent='Europe'
+
+#6
+SELECT name FROM world WHERE gdp > ALL(SELECT gdp FROM world WHERE continent='Europe' and gdp>0)
+
+#7
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+#8
+select continent, name from world x
+where name = (select name 
+                  from world y 
+                  where  x.continent =  y.continent 
+                  order by name asc 
+                  limit 1)
+      
+
